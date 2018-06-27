0e78a07c705bd995eb91b5fb6430c05e3e3a35d1
macro <ZHANGQUAN526@pingan.com.cn>
2018-06-27 16:26:28 +0800

    Initial Commit

---
+++ ZQDemoWeek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQDemoWeek
+//
+//  Created by 张泉(平安好房技术中心智慧城市房产云研发团队前端研发组) on 2018/6/27.
+//  Copyright © 2018年 张泉(平安好房技术中心智慧城市房产云研发团队前端研发组). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQDemoWeek/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZQDemoWeek
+//
+//  Created by 张泉(平安好房技术中心智慧城市房产云研发团队前端研发组) on 2018/6/27.
+//  Copyright © 2018年 张泉(平安好房技术中心智慧城市房产云研发团队前端研发组). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
