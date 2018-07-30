8ef0cca790652281df00c1b184e0fd6ed213b5f8
macro <ZHANGQUAN526@pingan.com.cn>
2018-07-30 17:45:46 +0800

    develop 修复 BUG

--- ZQDemoWeek/SubVC.m
+++ ZQDemoWeek/SubVC.m
@@ -18,15 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-
-    NSLog(@"develop修复BUG1");
-
-    NSLog(@"develop1 开发需求");
-
-
-
-
-    NSLog(@"develop2 修复需求");
+    NSLog(@"develop 修复 BUG");
 
 
 
