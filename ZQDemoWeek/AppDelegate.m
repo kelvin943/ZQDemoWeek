0c2390ea3dbccb97daebf4e85008ff86a95d6b55
macro <ZHANGQUAN526@pingan.com.cn>
2018-07-30 15:29:00 +0800

    开发分支修改

--- ZQDemoWeek/AppDelegate.m
+++ ZQDemoWeek/AppDelegate.m
@@ -31,6 +31,9 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"合并分支")
+    NSLog(@"合并分支修改")
+    NSLog(@"合并分支修改")
 }
 
 
